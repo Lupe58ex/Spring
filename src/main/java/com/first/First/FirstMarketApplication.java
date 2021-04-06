b356f2f70c867a4cb8b1e7e6bab680c93f356824
Lupe58ex <lu.58ex@gmail.com>
2021-04-05 20:28:08 -0500

    Add all code

---
+++ src/main/java/com/first/First/FirstMarketApplication.java
@@ -0,0 +1,13 @@
+package com.first.First;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/First/FirstMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.First;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
